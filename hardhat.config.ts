99a75b00a140de1dfd9c9fc20f23c82d24162ca1
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-12 16:40:50 +0000

    Added Goerli testnet for ethereum in hardhat.config And required packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+module.exports = {
+	solidity: '0.8.17',
+	networks: {
+		goerli: {
+			url: process.env.ALCHEMY_GOERLI_URL,
+			accounts: [process.env.GOERLI_PRIVATE_KEY],
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_KEY,
+	},
 };
-
-export default config;
