5fb1fb7bc5dc3ce5cf70a86c1823835d618bd864
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-12 16:59:17 +0000

    Added info comments to deployProxy script

--- scripts/deployProxy.js
+++ scripts/deployProxy.js
@@ -1,12 +1,12 @@
 const { ethers, upgrades } = require('hardhat');
 
 async function main() {
-	const VendingMachineV1 = await ethers.getContractFactory('VendingMachineV1');
-	const proxy = await upgrades.deployProxy(VendingMachineV1, [100]);
-	await proxy.deployed();
+	const VendingMachineV1 = await ethers.getContractFactory('VendingMachineV1'); // VendingMachineV1 is the name of the contract
+	const proxy = await upgrades.deployProxy(VendingMachineV1, [100]); // 100 is the initial supply
+	await proxy.deployed(); // Wait until the contract is deployed
 
 	const implementationAddress = await upgrades.erc1967.getImplementationAddress(
-		proxy.address
+		proxy.address // The address of the proxy contract
 	);
 
 	console.log('Proxy contract address: ' + proxy.address);
